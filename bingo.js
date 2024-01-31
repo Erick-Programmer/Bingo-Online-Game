0e8e118e09b4fe82c51537bef4c5d985a6f2c238
Carlos Erick Rosario <carloserickrosario@gmail.com>
2024-01-31 16:15:32 -0300

    Upload of html, css and js archives

---
+++ bingo.js
@@ -0,0 +1,22 @@
+//definir o numero aleatorio
+//numero aleatorio entre 1 e 99: função random!
+
+//preencher os numeros da cartela função random!
+//sair um numero atual função random!
+
+//ao sair o numero atual - aparece no visor!  
+//caso nao tenha saido nenhum numero - o espaço do numero anterior fica vazio. string = ""
+//vou ter 10 segundos pra marcar ou começa a outra rodada - aparecer o contador.
+
+
+//se eu clicar no numero da cartela que corresponde numero atual o botao fica inativo.
+////mas se eu clicar no numero da cartela que não corresponde numero atual o botao fica normal
+
+//ao preencher toda a cartela ganha o jogo - break no laço while - aparece vitoria (gera uma div com vitoria e permanece ate atualizar a pagina ou iniciar novamente o jogo)
+
+//numero 13 é um botao de desenho que nao serve pra nada (ele é inativo)
+
+//botao de iniciar começa o jogo - laço while - enquanto estiver executando - sai um numero - tempo pra marcar - somente encerra ao marcar toda cartela - break no laço.
+
+
+
